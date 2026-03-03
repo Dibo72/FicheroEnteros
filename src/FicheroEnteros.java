6b6d6f3be63557a8961f7f7f4b7c2ba32be1dff1
Dibo72 <humanida133@gmail.com>
2026-03-03 10:58:25 +0100

    Ejercicio acabado

--- src/FicheroEnteros.java
+++ src/FicheroEnteros.java
@@ -1,15 +1,48 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class FicheroEnteros {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        List<Integer> lista = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader("enteros.txt"))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                lista.add(Integer.parseInt(linea));
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
         }
+
+        System.out.println("Suma:");
+        int suma = 0;
+        for(int i : lista) {
+            suma += i;
+        }
+        System.out.println(suma);
+
+        System.out.println();
+        System.out.println("Maximo: ");
+        int maximo = 0;
+        for(int i : lista) {
+            if(i > maximo) {
+                maximo = i;
+            }
+        }
+        System.out.println(maximo);
+
+        System.out.println();
+
+        System.out.println("Minimo: ");
+        int minimo = lista.get(0);
+        for(int i : lista) {
+            if(i < minimo) {
+                minimo = i;
+            }
+        }
+        System.out.println(minimo);
     }
 }
